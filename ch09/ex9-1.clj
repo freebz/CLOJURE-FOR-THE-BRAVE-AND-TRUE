d8ca6f097b2e6224d5c69223d38754e67b4fcda3
FreeBz <freebz@hananet.net>
2018-01-25 10:22:57 +0900

    Cahpter 9: The Sacred Art of Concurrent and Parallel Programming

---
+++ ch09/ex9-1.clj
@@ -0,0 +1,17 @@
+;; Concurrency and Parallelism Concepts
+
+;; Managing Multiple Tasks vs. Executing Tasks Simultaneously
+
+;; Blocking and Asynchronous Tasks
+
+;; Concurrent Programming and Parallel Programming
+
+;; Clojure Implementation: JVM Threads
+
+;; What's a Thread?
+
+;; The Three Goblins: Reference Cells, Mutual Exclusion, and Dwarven Berserkers
+
+;; Futures, Delays, and Promises
+
+(web-api/get :dwarven-beard-waxes)
